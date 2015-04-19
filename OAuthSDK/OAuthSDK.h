dcc7c9aef4ec0912e4d20494443de449ea088dac
ramki1979 <chinnari@RamaKrishnas-Mac-mini.local>
2015-04-19 13:27:38 +0530

    Initial Commit

---
+++ OAuthSDK/OAuthSDK.h
@@ -0,0 +1,19 @@
+//
+//  OAuthSDK.h
+//  OAuthSDK
+//
+//  Created by RamaKrishna Mallireddy on 19/04/15.
+//  Copyright (c) 2015 VUContacts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OAuthSDK.
+FOUNDATION_EXPORT double OAuthSDKVersionNumber;
+
+//! Project version string for OAuthSDK.
+FOUNDATION_EXPORT const unsigned char OAuthSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OAuthSDK/PublicHeader.h>
+
+
